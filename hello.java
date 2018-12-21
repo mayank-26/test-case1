b42b4f8b6faaef8bc3dbfbbda807dd3012ff35dd
mayank-26 <42218630+mayank-26@users.noreply.github.com>
2018-12-22 02:34:06 +0530

    thank you added

--- hello.java
+++ hello.java
@@ -1,7 +1,8 @@
 class A
 {public static void main(String args[])
 {System.out.println(" hello!");
- System.out.println("bye");}}
+ System.out.println("bye");
+  System.out.println(" thanks");                         }}
 
 
 
