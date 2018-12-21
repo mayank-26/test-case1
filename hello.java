6acd15e591771de0c443e0643b5a63b57617b661
mayank-26 <42218630+mayank-26@users.noreply.github.com>
2018-12-22 02:38:39 +0530

    done

--- hello.java
+++ hello.java
@@ -2,7 +2,8 @@ class A
 {public static void main(String args[])
 {System.out.println(" hello!");
  System.out.println("bye");
-  System.out.println(" thanks");                         }}
+  System.out.println(" thanks");  
+/*done with editing/*                               }}
 
 
 
